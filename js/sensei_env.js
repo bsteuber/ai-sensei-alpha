06055eb08da0838b78648404ceed4ae1b9315daf
Benjamin Teuber <bsteuber@gmail.com>
2022-05-11 11:39:14 +0200

    Rank predict host

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -2,6 +2,7 @@ SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
   PATH_PREFIX: "ai-sensei-alpha",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/betaServer",
+  RANK_PREDICT_HOST:"http://35.192.78.242",
   WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
   ENABLE_LIVE_MODE: "true",
   ENABLE_FAST_LEVEL_UP: "true",
