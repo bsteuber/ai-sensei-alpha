e7a6fe00812cf22da6e9b4fa0d71eb4208d6e503
Benjamin Teuber <bsteuber@gmail.com>
2022-06-07 10:22:26 +0200

    "Update AI-Sensei"

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -1,11 +1,10 @@
 SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
-  PATH_PREFIX: "ai-sensei-alpha",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/betaServer",
   RANK_PREDICT_HOST:"https://ai-sensei.com/rank-predict",
   WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
-  ENABLE_LIVE_MODE: "true",
   ENABLE_FAST_LEVEL_UP: "true",
   ENABLE_WEAKER_BOTS: "true",
+  ENABLE_BUNDESLIGA_STATS: "true",
   READ_FROM_FIRESTORE_DURING_MIGRATION: "true"
 };
