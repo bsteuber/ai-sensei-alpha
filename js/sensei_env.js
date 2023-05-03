f5cdbf92e3826c56c3caabd93eb440d744169133
Benjamin Teuber <bsteuber@gmail.com>
2023-05-03 13:36:41 +0200

    "Update AI-Sensei"

--- js/sensei_env.js
+++ js/sensei_env.js
@@ -3,11 +3,11 @@ SENSEI_ENV = {
   FIREBASE_DB: "PRODUCTION",
   SERVER_HOST: "https://us-central1-sensei-160117.cloudfunctions.net/betaServer",
   RANK_PREDICT_HOST: "https://ai-sensei.com/rank-predict",
-  HUMANLIKE_HOST: "http://35.192.78.242/predict-humanlike",
+  HUMANLIKE_HOST: "https://ai-sensei.com/predict-humanlike",
+  ENABLE_HUMANLIKE_BOTS: "true",
   WS_SCHEDULER_URL: "wss://ai-sensei.com/ws",
   ENABLE_FAST_LEVEL_UP: "true",
   ENABLE_WEAKER_BOTS: "true",
-  ENABLE_HUMANLIKE_BOTS: "true",
   ENABLE_BUNDESLIGA_STATS: "true",
   READ_FROM_FIRESTORE_DURING_MIGRATION: "true",
   ENABLE_PANDANET_SIGNIN: "true"
